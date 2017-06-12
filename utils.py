bootlin/lavabo

commit 3d75976d08b3765242ef67c00b65672448be1d68

lavabo: make autocompletion faster
Instead of asking the list of devices with all data, which requires <number
of devices>+1 queries, use a single query.

Signed-off-by: Quentin Schulz <quentin.schulz@free-electrons.com>

lavabo-server
@@ -265,6 +265,8 @@ def handle(data, stdout):
         ans = utils.create_answer("error", "Missing board name.")
         if "list" in data:
             ans = utils.create_answer("success", list_devices(db_cursor))
+        if "simple-list" in data:
+            ans = utils.create_answer("success", utils.get_simple_device_list(proxy))
         elif "upload" in data:
             ans = utils.create_answer("success", str(os.path.join(args.tftp_dir, user)))
         #This is status from LAVA, offline_by will always be "daemon"

utils.py
@@ -30,6 +30,8 @@ import urlparse
 import xmlrpclib
 import re
 
+def get_simple_device_list(proxy):
+    proxy.scheduler.all_devices()
 
 def get_device_list(db_conn, proxy):
     devices = {}

lavabo
@@ -37,7 +37,7 @@ def board_completer(prefix, parsed_args, **kwargs):
     hostname = config_parser.get("lavabo-server", "hostname")
     user = config_parser.get("lavabo-server", "user")
     port = config_parser.getint("lavabo-server", "port")
-    answer = send_message("list", user, hostname, port)
+    answer = send_message("simple-list", user, hostname, port)
     try:
         answer = json.loads(answer)
     except ValueError as e:
@@ -46,7 +46,7 @@ def board_completer(prefix, parsed_args, **kwargs):
     if answer["status"] != "success":
         argcomplete.warn("error: %s" % answer["content"])
         sys.exit(1)
-    return [board['hostname'] for board in answer["content"] if board['hostname'].startswith(prefix)]
+    return [board[0] for board in answer["content"] if board[0].startswith(prefix)]
 
 parser = argparse.ArgumentParser(description="Client to connect to lavabo-server used to remote control boards in LAVA.")
 parser.add_argument("-c", "--conf-file", type=argparse.FileType("r"), default=os.path.join(os.path.expanduser("~"), ".lavabo.conf"), help="the location of lavaboconfiguration file. Default: $HOME/.lavabo.conf.")
@@ -82,7 +82,7 @@ parser_where.add_argument("BOARD", nargs="?", default=os.environ.get("LAVABO_BOA
 def send_message(cmd, user, hostname, port, board=None):
     ssh = subprocess.Popen(("ssh -q %s@%s -p %d interact" % (user, hostname, port)).split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-    if cmd in ["list", "update"]:
+    if cmd in ["list", "update", "simple-list"]:
         msg = json.dumps({cmd: ""})
     else:
         msg = json.dumps({cmd: {"board": board}})

commit d03c8372cdd2473e38875e481d813a27cb3f5e08

lavabo-server: fix autocompletion
Forgot the return statement and an elif instead of an if resulting in
broken `lavabo list` and in autocompletion not working at all.

Fixes: 3d75976d08b3 ("lavabo: make autocompletion faster")

Signed-off-by: Quentin Schulz <quentin.schulz@free-electrons.com>

lavabo-server
@@ -265,7 +265,7 @@ def handle(data, stdout):
         ans = utils.create_answer("error", "Missing board name.")
         if "list" in data:
             ans = utils.create_answer("success", list_devices(db_cursor))
-        if "simple-list" in data:
+        elif "simple-list" in data:
             ans = utils.create_answer("success", utils.get_simple_device_list(proxy))
         elif "upload" in data:
             ans = utils.create_answer("success", str(os.path.join(args.tftp_dir, user)))

utils.py
@@ -31,7 +31,7 @@ import xmlrpclib
 import re
 
 def get_simple_device_list(proxy):
-    proxy.scheduler.all_devices()
+    return proxy.scheduler.all_devices()
 
 def get_device_list(db_conn, proxy):
     devices = {}